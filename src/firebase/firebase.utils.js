058697fd310bd5f456f1c5992d97eaa148a0a061
AurionLab <aurion@student.telkomuniversity.ac.id>
2020-01-07 13:18:06 +0700

    Lesson 26: Adding collection to firebase and pull in our app collections

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,25 +3,23 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyAT4K6J_0CChVALIK4P1lE55HFuQ9Z7iE8",
-  authDomain: "crwn-db-ffd61.firebaseapp.com",
-  databaseURL: "https://crwn-db-ffd61.firebaseio.com",
-  projectId: "crwn-db-ffd61",
-  storageBucket: "crwn-db-ffd61.appspot.com",
-  messagingSenderId: "747787249293",
-  appId: "1:747787249293:web:6269f7b95bf9ec723aa4c4",
-  measurementId: "G-LWGH6DW120"
+  apiKey: "AIzaSyCoKey_zuOkMEmVCmC7xy91OGLhwhVbhzc",
+  authDomain: "crwn-db-dc449.firebaseapp.com",
+  databaseURL: "https://crwn-db-dc449.firebaseio.com",
+  projectId: "crwn-db-dc449",
+  storageBucket: "crwn-db-dc449.appspot.com",
+  messagingSenderId: "961035834696",
+  appId: "1:961035834696:web:53b242aff63f646dc88f7d",
+  measurementId: "G-FBGM1FM3GM"
 };
 
 firebase.initializeApp(config);
 
+// Firebase authentification
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
-
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-
   const snapShot = await userRef.get();
-
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -38,6 +36,24 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+// Firebase Add Collection and Document
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
