c73b8ff46b25e232f936d81dd917f31e03d53baa
AurionLab <aurion@student.telkomuniversity.ac.id>
2020-01-28 13:12:10 +0700

    final for stripe

---
+++ client/build/precache-manifest.e5b76fd0f381037439609cb398fc3ac0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa89fcc6242e63338ba3345a5966bae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "643004e9ee6a8b6204d7",
+    "url": "/static/css/main.0126da47.chunk.css"
+  },
+  {
+    "revision": "2bc41098e33868219340",
+    "url": "/static/js/2.718c2696.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.718c2696.chunk.js.LICENSE"
+  },
+  {
+    "revision": "643004e9ee6a8b6204d7",
+    "url": "/static/js/main.baf9f718.chunk.js"
+  },
+  {
+    "revision": "edc818eeb98ec2044b54",
+    "url": "/static/js/runtime-main.9a8e9cbd.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
